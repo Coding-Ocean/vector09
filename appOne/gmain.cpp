dd6fa93bc881690380c560c308ce67ab98924d22
Coding-Ocean <eijo524@gmail.com>
2021-03-31 08:39:52 +0900

    comp

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -1,6 +1,56 @@
 #include"libOne.h"
 void gmain() {
     window(1000, 1000);
+    float x, y, r;
+    float s, c;
+    while (notQuit) {
+        clear(0, 50, 0);
+        mathAxis(3.1f,255);
+        //マウス座標をベクトルとする
+        x = mathMouseX;
+        y = mathMouseY;
+        //正規化ベクトルを求める
+        r = sqrt(x * x + y * y);
+        c = x / r;
+        s = y / r;
+        //元のベクトルを描画
+        strokeWeight(4);
+        stroke(128);
+        mathArrow(0, 0, x, y);
+        //単位円を描画
+        fill(0, 0, 0, 0);
+        mathCircle(0, 0, 2);
+        //正規化ベクトルを描画
+        strokeWeight(10);
+        stroke(200);
+        mathArrow(0, 0, c, s);
+        //cosθを描画
+        stroke(160, 200, 255);
+        mathLine(0, 0, c, 0);
+        //sinθを描画
+        stroke(255, 200, 200);
+        mathLine(c, 0, c, s);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#if 1
+#else
+#include"libOne.h"
+void gmain() {
+    window(1000, 1000);
     angleMode(DEGREES);
     colorMode(HSV);
     float x, y, r;
@@ -48,3 +98,4 @@ void gmain() {
         text((let)"tangent=" + tangent, 0, 150);
     }
 }
+#endif
